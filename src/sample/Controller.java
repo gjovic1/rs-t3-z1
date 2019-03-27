7d37ec6dfc8ce3b2fcf8fcf385ff407dddcc9b62
gjovic1 <gjovic1@etf.unsa.ba>
2019-03-27 09:57:02 +0100

    Zadatak 1, Tutorijal 3

---
+++ src/sample/Controller.java
@@ -0,0 +1,51 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Controller {
+    public Button dugmic;
+    public TextField Unos;
+    public ListView listaIspis;
+
+
+    public void initialize() {
+        Unos.getStyleClass().add("poljeNijeIspravno");
+        Unos.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (n.isEmpty()) {
+                    Unos.getStyleClass().add("poljeNijeIspravno");
+                } else {
+                    Unos.getStyleClass().removeAll("poljeNijeIspravno");
+                }
+            }
+        });
+    }
+
+
+
+    public static int sumaCifara(int broj) {
+        int sum = 0;
+        while (broj != 0) {
+            sum = sum + (broj % 10);
+            broj = broj / 10;
+        }
+        return sum;
+    }
+
+    public void print(ActionEvent actionEvent) throws InvocationTargetException {
+
+        String n = Unos.getText();
+        for (int i = 1; i < Integer.parseInt(n); i++)
+            if (i % sumaCifara(i) == 0)
+                listaIspis.getItems().add(i);
+    }
+}
